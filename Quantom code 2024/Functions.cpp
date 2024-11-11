9c3d55221aa5bb0a82a62ed488f43033684d9abe
Ksturmanov23 <Ksturmanov23@codingburgas.bg>
2024-11-11 18:55:17 +0200

    Create rules template

--- Quantom code 2024/Functions.cpp
+++ Quantom code 2024/Functions.cpp
@@ -1,6 +1,6 @@
 #include "Functions.h"
 #include <iostream>
-
+using namespace std;
 void startNumberWordle()
 {
 
@@ -8,17 +8,76 @@ void startNumberWordle()
 
 void startGuessTheNumber()
 {
+	cout << "                         ------------------------------------------------------------------                        \n";
+	cout << "                        |                           Number Puzzle                          |\n";
+	cout << "                        |                                                                  |\n";
+	cout << "                        |                      The Rules of the game are:                  |\n";
+	cout << "                        |                                                                  |\n";
+	cout << "                        |  There is a text that you have to read carefully.                |\n";
+	cout << "                        |  When you read the text, you have to find the number 'X'.        |\n";
+	cout << "                        |  When you find the number X, please only answer with that        |\n";
+	cout << "                        |  number.                                                         |\n";
+	cout << "                        |  You have 3 tries to find the number.                            |\n";
+	cout << "                        |                                                                  |\n";
+	cout << "                        |                            Good luck!                            |\n";
+	cout << "                         ------------------------------------------------------------------                         \n";
+
+
 }
 
 void startNumberPuzzle()
 {
+	cout << "                         ------------------------------------------------------------------                        \n";
+	cout << "                        |                           TEMPLATE                               |\n";
+	cout << "                        |                                                                  |\n";
+	cout << "                        |                      The Rules of the game are:                  |\n";
+	cout << "                        |                                                                  |\n";
+	cout << "                        |  There is a text that you have to read carefully.                |\n";
+	cout << "                        |  When you read the text, you have to find the number 'X'.        |\n";
+	cout << "                        |  When you find the number X, please only answer with that        |\n";
+	cout << "                        |  number.                                                         |\n";
+	cout << "                        |  You have 3 tries to find the number.                            |\n";
+	cout << "                        |                                                                  |\n";
+	cout << "                        |                            Good luck!                            |\n";
+	cout << "                         ------------------------------------------------------------------                         \n";
+	
+
 }
 
 void startMathsTest()
 {
+	cout << "                         ------------------------------------------------------------------                        \n";
+	cout << "                        |                           TEMPLATE                               |\n";
+	cout << "                        |                                                                  |\n";
+	cout << "                        |                      The Rules of the game are:                  |\n";
+	cout << "                        |                                                                  |\n";
+	cout << "                        |  There is a text that you have to read carefully.                |\n";
+	cout << "                        |  When you read the text, you have to find the number 'X'.        |\n";
+	cout << "                        |  When you find the number X, please only answer with that        |\n";
+	cout << "                        |  number.                                                         |\n";
+	cout << "                        |  You have 3 tries to find the number.                            |\n";
+	cout << "                        |                                                                  |\n";
+	cout << "                        |                            Good luck!                            |\n";
+	cout << "                         ------------------------------------------------------------------                         \n";
+
+
 }
 
 void aboutUs(){
+	cout << "                         ------------------------------------------------------------------                        \n";
+	cout << "                        |                           TEMPLATE                               |\n";
+	cout << "                        |                                                                  |\n";
+	cout << "                        |                      The Rules of the game are:                  |\n";
+	cout << "                        |                                                                  |\n";
+	cout << "                        |  There is a text that you have to read carefully.                |\n";
+	cout << "                        |  When you read the text, you have to find the number 'X'.        |\n";
+	cout << "                        |  When you find the number X, please only answer with that        |\n";
+	cout << "                        |  number.                                                         |\n";
+	cout << "                        |  You have 3 tries to find the number.                            |\n";
+	cout << "                        |                                                                  |\n";
+	cout << "                        |                            Good luck!                            |\n";
+	cout << "                         ------------------------------------------------------------------                         \n";
+
 
 }
 
